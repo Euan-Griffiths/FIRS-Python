352d65f9c6d7d7d8e66d684acc76878b4cfd0ef1
euan-griffiths <griffithseuan01@icloud.com>
2025-12-11 11:34:04 +0000

    Moved Old Code into Archive

---
+++ ARCHIVE/astropy/open.py
@@ -0,0 +1,18 @@
+from astropy.io import fits
+from matplotlib import pyplot as plt
+import numpy
+
+hdul = fits.open(r".\astropy\502nmos.fits")
+info = hdul[0].header
+data = hdul[0].data
+
+print(data.shape)
+
+#data = numpy.reshape(data,(1600, 1600))
+print(f"Data Shape (NAXIS,NAXIS1): {data.shape}")
+
+plt.imshow(data, cmap="CMRmap",vmin=0,vmax=70)
+plt.colorbar()
+plt.show()
+input()
+hdul.close()
